e6b2f29d87b18ea5d33b99e2bf842881ab41f304
Brian Pugh <bnp117@gmail.com>
2018-04-04 09:01:54 -0700

    Added insufficient buffer test to nl_public_to_address

--- test/test_nano_lib.c
+++ test/test_nano_lib.c
@@ -106,22 +106,33 @@ TEST_CASE("Public Address To Public Key", "[nano_lib]"){
 
 TEST_CASE("Public Key To Public Address", "[nano_lib]"){
     char guess_address[ADDRESS_BUF_LEN];
+    char short_buffer[10];
     uint256_t test_public_key_bin;
     nl_err_t res;
 
-    /* Test 1 */
+    /* Test 1 - Normal Use*/
     sodium_hex2bin(test_public_key_bin, sizeof(test_public_key_bin), \
             "68D2CEA554187DDF4891E2BDC7AB7442F230A650826455411401B41EEC9BED31",
             HEX_256, NULL, NULL, NULL);
     res = nl_public_to_address(guess_address,
             sizeof(guess_address),
             test_public_key_bin);
-    if(res != E_SUCCESS){
-        TEST_FAIL_MESSAGE("nl_public_to_address returned an unsuccessful code");
-    }
+    TEST_ASSERT_EQUAL_INT_MESSAGE(E_SUCCESS, res,
+        "nl_public_to_address returned an unsuccessful code");
     TEST_ASSERT_EQUAL_STRING(
             "xrb_1t8kstkoa85xux6b5roxryoqaiqk84m731m6co1ja1fn5upbqubj34osorm9",
             guess_address);
+
+    /* Test 1 - Insufficient buffer to hold address*/
+    sodium_hex2bin(test_public_key_bin, sizeof(test_public_key_bin), \
+            "68D2CEA554187DDF4891E2BDC7AB7442F230A650826455411401B41EEC9BED31",
+            HEX_256, NULL, NULL, NULL);
+    res = nl_public_to_address(short_buffer,
+            sizeof(short_buffer),
+            test_public_key_bin);
+    TEST_ASSERT_EQUAL_INT_MESSAGE(E_INSUFFICIENT_BUF, res,
+            "nl_public_to_address didn't return insufficient buffer error "
+            "for a too short buffer.");
 }
 
 TEST_CASE("Private Key To Public Key", "[nano_lib]"){
@@ -130,13 +141,13 @@ TEST_CASE("Private Key To Public Key", "[nano_lib]"){
     uint256_t guess_public_key_bin;
     hex256_t guess_public_key_hex;
 
+    /* Test Case 1 - Normal Use */
     sodium_hex2bin(test_private_key_bin, sizeof(test_private_key_bin), \
             "102A1BD8E50D314B1AF18B064763836500961D97E1517B409D9797E37F148290",
             HEX_256, NULL, NULL, NULL);
 	res = nl_private_to_public(guess_public_key_bin, test_private_key_bin);
-    if(res != 0){
-        TEST_FAIL_MESSAGE("nl_private_to_public returned an unsuccessful code");
-    }
+    TEST_ASSERT_EQUAL_INT_MESSAGE(E_SUCCESS, res,
+            "nl_private_to_public returned an unsuccessful code");
     sodium_bin2hex(guess_public_key_hex, sizeof(guess_public_key_hex),
             guess_public_key_bin, sizeof(guess_public_key_bin));
     strupper(guess_public_key_hex);
