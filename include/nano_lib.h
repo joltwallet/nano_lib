ade6319408ddc6cbb1ba4c07bb4a3100fa8106de
James Coxon <175479+jamescoxon@users.noreply.github.com>
2018-06-20 06:42:33 +0100

    change license from GPL to MIT, removed boilerplate code

--- include/nano_lib.h
+++ include/nano_lib.h
@@ -1,18 +1,6 @@
 /* nano_lib - ESP32 Any functions related to seed/private keys for Nano
  Copyright (C) 2018  Brian Pugh, James Coxon, Michael Smaili
  https://www.joltwallet.com/
- 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software Foundation,
- Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
 #ifndef __NANO_LIB_H__
